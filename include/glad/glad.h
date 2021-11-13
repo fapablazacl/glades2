2e7336b28967f60e5fcbd18ed62ecf8f9397fab9
Felipe Apablaza <ing.apablaza@gmail.com>
2021-11-13 09:31:51 -0300

    first commit

---
+++ include/glad/glad.h
@@ -0,0 +1,6 @@
+
+#if defined(_DEBUG)
+#include <glad/glad_debug.h>
+#else
+#include <glad/glad_release.h>
+#endif
---
+++ src/glad.c
@@ -0,0 +1,6 @@
+
+#if defined(_DEBUG)
+#include "glad_debug_impl.h"
+#else
+#include "glad_release_impl.h"
+#endif
